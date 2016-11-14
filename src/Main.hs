5ba08545508ae44058d3f4a7ed5881d1e92ee433
Max Taldykin <jorpic@gmail.com>
2016-11-14 20:40:10 +0300

    Render latex templates

--- src/Main.hs
+++ src/Main.hs
@@ -3,15 +3,23 @@
 
 module Main where
 
-import           Control.Monad (void)
+import           Control.Monad (guard, filterM)
+import           Control.Lens ((.~))
 
+import           Data.List (isSuffixOf)
 import qualified Data.Aeson as Aeson
-import qualified Data.HashMap.Strict as HM
 import qualified Data.Configurator as Config
 
+
 import           System.IO (hPutStrLn, stderr)
+import           System.Directory (listDirectory, doesFileExist)
+import           System.FilePath ((</>))
 import qualified System.Environment as Env
 import qualified System.Process as P
+import           System.Exit
+
+import qualified Data.Text.Lazy.IO as TL
+import qualified Text.EDE as EDE
 
 import GHC.Generics
 
@@ -35,17 +43,59 @@ main = do
         <*> Config.require cfg "result_dir"
         <*> Config.require cfg "render_cmd"
       main' config
-    _ -> hPutStrLn stderr $ "Usage: " ++ prog ++ " <config.conf>"
+    _ -> report $ "Usage: " ++ prog ++ " <config.conf>"
 
 
 main' :: Config -> IO ()
 main' (Config {..}) = do
-  (code, sOut, sErr) <- P.readCreateProcessWithExitCode
+  let emptyObject = EDE.fromPairs []
+
+  renderTemplates templateDir emptyObject
+    >>= guard . all id
+
+  (code, _sOut, _sErr) <- P.readCreateProcessWithExitCode
     (P.shell renderCmd) {P.cwd = Just templateDir}
     ""
+  case code of
+    ExitFailure _err -> return ()
+    ExitSuccess -> return ()
 
---  (_, hOut, _, hProc) <- P.createProcess (P.shell renderCmd)
---    { P.cwd = Just templateDir
---    , P.std_out = P.CreatePipe
---    }
---  void $ P.waitForProcess hProc
+
+-- FIXME: move ".tex" to config?
+-- | Render *.tex templates in `dir`.
+renderTemplates :: FilePath -> Aeson.Object -> IO [Bool]
+renderTemplates dir obj
+  = forEachFile ".tex" dir (\filePath -> do
+    putStrLn filePath
+    EDE.parseFileWith latexSyntax filePath >>= \case
+      EDE.Failure err -> report (show err) >> return False
+      EDE.Success tpl -> case EDE.eitherRender tpl obj of
+        Left err  -> report err >> return False
+        Right txt -> TL.writeFile filePath txt >> return True
+    )
+
+
+-- | Shallow scan `dir` for files with `ext` and perform `op` on each of them.
+forEachFile :: String -> FilePath -> (FilePath -> IO a) -> IO [a]
+forEachFile ext dir op
+  =   listDirectory dir
+  >>= return . map (dir </>) . filter (isSuffixOf ext)
+  >>= filterM doesFileExist
+  >>= mapM op
+
+
+latexSyntax :: EDE.Syntax
+latexSyntax
+  = EDE.delimInline  .~ ("\\param{", "}")
+  $ EDE.delimPragma  .~ ("%% !EDE:", "\n")
+  $ EDE.delimBlock   .~ ("%% %EDE:", "\n")
+  $ EDE.delimComment .~ ("%% #EDE:", "\n")
+  $ EDE.defaultSyntax
+
+
+is :: Bool -> Bool
+is = id
+
+
+report :: String -> IO ()
+report = hPutStrLn stderr
